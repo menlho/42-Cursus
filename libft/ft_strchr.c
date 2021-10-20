ab12b19b9bdde92550c48e3886dd1ece9e4bf97f
omaii <nangiie.t@gmail.com>
2021-10-20 21:08:40 +0200

    strchr done

---
+++ libft/ft_strchr.c
@@ -0,0 +1,14 @@
+char *ft_strchr(char *str, int chr)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+    {
+        if str[i] == c
+            return (str[i]);
+        i++;
+    }
+    if c == '\0'
+        return (str[i]);
+}
